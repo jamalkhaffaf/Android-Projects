42f31317974b0ba8c449c6f75d4e8d7290d83c93
jamalkhaffaf <jamalkhaffaf@gmail.com>
2015-05-14 19:39:11 -0700

    Creating the db part for Homework2. Still in progress

--- Android Class 250/Homework2/app/src/main/java/com/example/jamalj/homework2/data/TaskDbHelper.java
+++ Android Class 250/Homework2/app/src/main/java/com/example/jamalj/homework2/data/TaskDbHelper.java
@@ -1,8 +1,14 @@
 package com.example.jamalj.homework2.data;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import com.example.jamalj.homework2.model.Task;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by JamalJ on 5/13/2015.
@@ -27,12 +33,14 @@ public class TaskDbHelper extends SQLiteOpenHelper {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
+    //db create
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(TASK_TABLE_CREATE);
-        log.i(LOG_TAG, "Creating table with query: " + TASK_TABLE_CREATE);
+        Log.i(LOG_TAG, "Creating table with query: " + TASK_TABLE_CREATE);
     }
 
+    //db upgrade
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP " + TASK_TABLE_NAME);
@@ -40,8 +48,84 @@ public class TaskDbHelper extends SQLiteOpenHelper {
     }
 
     //add a task to the database
-    public void addTask(String taskName, String taskDesc){
+    public void addTask(String taskName, String taskDesc) {
 
+        Log.i(LOG_TAG, "Added a task: " + taskName);
+
+        // Get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        // Create ContentValues to add data
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TASK_ROW_NAME, taskName);
+        contentValues.put(TASK_ROW_DESC, taskDesc);
+
+        // Insert data to table
+        db.insert(TASK_TABLE_NAME, // table name
+                null,
+                contentValues);
+
+        // Remember to close the db
+        db.close();
+    }
+
+    public List<Task> getTasks() {
+
+        // Get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        List<Task> tasks = new ArrayList<Task>();
+
+        Cursor cursor = db.query(TASK_TABLE_NAME,
+                TaskContract.Task.PROJECTION,
+                null,
+                null,
+                null,
+                null,
+                null);
+
+        cursor.moveToFirst();
+
+        while (!cursor.isAfterLast()) {
+            Task task= cursorToTask(cursor);
+            tasks.add(task);
+            cursor.moveToNext();
+        }
+
+        cursor.close();
+        db.close();
+
+        return tasks;
+    }
+
+    //utilize courser to be converted into task object and be used to retrieve data
+    private Task cursorToTask(Cursor cursor) {
+
+        Task task = new Task();
+        task.setId(cursor.getLong(0));
+        task.setName(cursor.getString(1));
+        task.setDesc(cursor.getString(2));
+
+        return task;
+    }
+
+    //db delete one record
+    public int deleteTask(long id) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        int count = db.delete(TASK_TABLE_NAME, TASK_ROW_ID + " = " + id, null);
+        db.close();
+        return count;
+    }
+
+    //db delete multi records
+    public void deleteTasks(ArrayList<Integer> ids) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        for (long id: ids) {
+            db.delete(TASK_TABLE_NAME, TASK_ROW_ID + " = " + id, null);
+        }
+        db.close();
     }
 
 }
